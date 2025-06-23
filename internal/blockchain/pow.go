3220fb5b12ae682472f7353c37edd6c61fcd715c
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-06-23 23:06:50 +0300

    WARN: unchecked commit

--- internal/blockchain/pow.go
+++ internal/blockchain/pow.go
@@ -7,9 +7,10 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
-const Difficulty = 6
+const Difficulty = 4
 
 func (bc *Blockchain) MineBlock(t transaction.Transaction, publicKey *rsa.PublicKey, signature string) {
 	if encryption.VerifySignature(publicKey, []byte(t.ToString()), []byte(signature)) != nil {
@@ -21,15 +22,20 @@ func (bc *Blockchain) MineBlock(t transaction.Transaction, publicKey *rsa.Public
 	fmt.Println("⛏️ Mining...")
 	newBlock := generateBlock(*bc.LastBlock(), t)
 
+	start := time.Now()
 	for {
 		hash := encryption.Hash(newBlock.computeHash() + strconv.Itoa(newBlock.Nonce))
 		
 		if hash[:Difficulty] == strings.Repeat("0", Difficulty) {
-			fmt.Printf("Hash found: %s\n\n", hash)
+			fmt.Printf("Hash found: %s\n", hash)
 			break
 		}
 		newBlock.Nonce++
 	}
+	end := time.Now()
+
+	elapsed := end.Sub(start)
+	fmt.Println("Time elapsed: ", elapsed)
 
 	bc.AddBlock(newBlock)
 }
